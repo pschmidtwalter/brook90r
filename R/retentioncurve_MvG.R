0016b1c36582023d96508269ff43e156fbccb625
Paul Schmidt-Walter <pschmidtwalter@nfv.nw-fva.de>
2018-12-17 14:07:43 +0100

    Version update

--- R/retentioncurve_MvG.R
+++ R/retentioncurve_MvG.R
@@ -1,13 +1,14 @@
 #' Retention-Curve: water content vs. pressure head, van Genuchten
 #'
 #' @param psi vector of pressure heads
-#' @param alpha value vG-alpha, unit: reciprocal pressure head
-#' @param n value of van Genuchten parameter N
-#' @param ThS n value of van Genuchten parameter Ths (saturation water content)
-#' @param ThR n value of van Genuchten parameter Thr residual water content
-#' @param m value of van Genuchten parameter N. Defaults 1-1/n
+#' @param alpha van Genuchten alpha, unit: reciprocal pressure head
+#' @param n van Genuchten parameter N
+#' @param ThS van Genuchten parameter Ths (saturation water content)
+#' @param ThR van Genuchten parameter Thr (residual water content)
+#' @param m van Genuchten parameter M. Default: 1-1/n
 #'
 #' @return vector of water contents
+#' @export
 MvG.swc <- function(
   psi, #pressure head in hPa
   alpha, #MvG alpha
@@ -17,6 +18,6 @@ MvG.swc <- function(
   m = 1-1/n)
 {
   wetness <- 1/((1 + (alpha * psi)^n))^(m)
-  theta <- wetness * (ThS-ThR) +ThR
+  theta <- wetness * (ThS-ThR) + ThR
   return(theta)
 }
